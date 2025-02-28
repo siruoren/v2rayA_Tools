5b977e224a839a546aabe1cf72a376233774106d
Your Name <you@example.com>
2025-02-28 10:20:49 +0800

    add logs out

--- restart_con_sub.sh
+++ restart_con_sub.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd $(dirname $0);
-
-python3 updateSub.py > restart.log
+echo `date +%y%m%d%H%M%S` > restart.log
+python3 updateSub.py >> restart.log
 
 python3 main.py >> restart.log 
+echo `date +%y%m%d%H%M%S` >> restart.log

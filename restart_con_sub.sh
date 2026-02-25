0158b8d734b40153a6457dab6b5fc3d7abfb8be4
siruoren <zhuguojun6@163.com>
2026-02-25 14:01:17 +0800

    add

--- restart_con_sub.sh
+++ restart_con_sub.sh
@@ -2,7 +2,9 @@
 cd $(dirname $0);
 mkdir -p logs;
 echo "START TIME: `date`" > logs/restart.log
-python3 updateSub.py 2>&1|tee logs/restart.log
+python3 delSub.py 2>&1|tee -a logs/restart.log
+python3 addSub.py 2>&1|tee -a logs/restart.log
+python3 updateSub.py 2>&1|tee -a logs/restart.log
 
-python3 main.py  2>&1|tee logs/restart.log 
-echo "END TIME: `date`"  >> logs/restart.log
+python3 main.py  2>&1|tee -a logs/restart.log 
+echo "END TIME: `date`"  >> -a logs/restart.log

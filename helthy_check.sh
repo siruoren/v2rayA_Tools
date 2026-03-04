3e091e7c93c156010250960c0778618d5d0a5bd8
siruoren <zhuguojun6@163.com>
2026-03-04 17:51:24 +0800

    fix(health_check): 优化健康检查脚本并添加时间戳
    检查curl是否已安装，避免重复安装
    添加开始和结束检查的时间戳
    移除注释掉的telegram检查代码

--- helthy_check.sh
+++ helthy_check.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 set -e;
-apk add curl;
+if [[ `curl -V|wc -l` == 0 ]];then
+    apk add curl;
+fi
+echo "###[date: `date +%Y%m%d-%H:%M:%S`] begin check......"
 curl -L -k --socks5 127.0.0.1:10800 --connect-timeout 5 https://www.google.com||exit 1
+echo ''
+#curl -L -k --socks5 127.0.0.1:10800 --connect-timeout 5 https://telegram.org||exit 1
+echo ''
+echo "###[date: `date +%Y%m%d-%H:%M:%S`] end check."

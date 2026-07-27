12b3e10826615db73482fc67a8ee266ae45778f2
siruoren <zhuguojun6@163.com>
2026-07-27 17:48:54 +0800

    Revert "fix: 修复日志记录命令，确保正确追加结束时间"
    This reverts commit 8ed0f3551b246f9bc94815910abee1fada8c141b.

--- reset_proxy.sh
+++ reset_proxy.sh
@@ -7,4 +7,4 @@ python3 addSub.py 2>&1|tee -a logs/restart.log
 python3 updateSub.py 2>&1|tee -a logs/restart.log
 
 python3 main.py  2>&1|tee -a logs/restart.log 
-echo "END TIME: `date`"  |tee -a logs/restart.log
+echo "END TIME: `date`"  >> -a logs/restart.log
